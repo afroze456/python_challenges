2f4424c0f8275a4b59e706f7192815619d35897a
afroze456 <afreenafroze1@gmail.com>
2026-05-03 14:29:43 -0700

    Add files via upload

---
+++ activty2.py
@@ -0,0 +1,9 @@
+print("enter a numerator")
+numn=int(input())
+print("enter a denomeator")
+numd=int(input())
+
+if numn%numd==0:
+    print("the number is divisable")
+else:
+    print("the number is not divisable")
