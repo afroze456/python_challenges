eec5f8fd10c3288ddc044b6901651bcf84b97518
afroze456 <afreenafroze1@gmail.com>
2026-05-03 14:25:18 -0700

    Add files via upload

---
+++ activty1.py
@@ -0,0 +1,18 @@
+v= 4
+w= 5
+x= 8
+y= 2
+z= 0
+z=(v+w)*x/y;
+print("the value of (v+w)*x/y is equal to ", z)
+
+
+
+name= "Alex"
+age= 0
+
+
+if name =="Alex" or name== "john" and age >= 2:
+    print("hello welcome")
+else:
+        print("GoodBye!!")
